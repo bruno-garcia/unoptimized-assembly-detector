c4938a4a180632e4adcd201be55de58f05cb47c0
Bruno Garcia <bruno@brunogarcia.com>
2021-05-20 00:09:58 -0400

    dont infer from byte array value

--- src/UnoptimizedAssemblyDetector/DetectUnoptimizedAssembly.cs
+++ src/UnoptimizedAssemblyDetector/DetectUnoptimizedAssembly.cs
@@ -18,7 +18,7 @@ namespace UnoptimizedAssemblyDetector
     public class DetectUnoptimizedAssembly : MSBuildTask
     {
         /// <summary>
-        /// A list of referenced assembly paths split by semi colon. 
+        /// A list of referenced assembly paths split by semi colon.
         /// </summary>
        [Required] public string ReferencePath { get; set; } = null!;
 
@@ -29,7 +29,6 @@ namespace UnoptimizedAssemblyDetector
 
         public override bool Execute()
         {
-            // Thread.Sleep(10000);
             var outputFileDll = $"{AssemblyName}.dll";
             var outputFileExe = $"{AssemblyName}.exe";
             var assemblyPaths = ReferencePath.Split(';');
@@ -37,101 +36,64 @@ namespace UnoptimizedAssemblyDetector
             {
                 var file = Path.GetFileName(assemblyPath);
                 if (file.StartsWith("System")
-                    // || file.StartsWith("Microsoft")
+                    || file.StartsWith("Microsoft")
                     || assemblyPath.Contains("NETCore.App.Ref")
-                    // || file.Equals(outputFileDll, StringComparison.InvariantCultureIgnoreCase)
-                    // || file.Equals(outputFileExe, StringComparison.InvariantCultureIgnoreCase)
-                    )
+                    || file.Equals(outputFileDll, StringComparison.InvariantCultureIgnoreCase)
+                    || file.Equals(outputFileExe, StringComparison.InvariantCultureIgnoreCase))
                 {
                     continue;
                 }
+
                 Log.LogMessage(MessageImportance.Low, "Checking if assembly is optimized: {0}", assemblyPath);
 
                 using var stream = File.OpenRead(assemblyPath);
                 using var reader = new PEReader(stream);
                 var metadata = reader.GetMetadataReader();
                 var assembly = metadata.GetAssemblyDefinition();
-                foreach (var attribute in assembly.GetCustomAttributes())
+                var isOptimized = true;
+                foreach (var customAttributeTypedArgument in
+                    from attribute in assembly.GetCustomAttributes()
+                    where !attribute.IsNil
+                    select metadata.GetCustomAttribute(attribute)
+                    into customAttribute
+                    let ctor = metadata.GetMemberReference((MemberReferenceHandle)customAttribute.Constructor)
+                    where metadata.GetString(metadata.GetTypeReference((TypeReferenceHandle)ctor.Parent).Name) is "DebuggableAttribute"
+                    select customAttribute.DecodeValue(new CustomAttributeTypeProvider()) into value
+                    from customAttributeTypedArgument in value.FixedArguments
+                    select customAttributeTypedArgument)
                 {
-                    if (attribute.IsNil)
+                    if (!int.TryParse(customAttributeTypedArgument.Value?.ToString(), out var bitmask))
                     {
                         continue;
                     }
-                    var customAttribute = metadata.GetCustomAttribute(attribute);
-                    var ctor = metadata.GetMemberReference((MemberReferenceHandle)customAttribute.Constructor);
-                    var attrType = metadata.GetTypeReference((TypeReferenceHandle)ctor.Parent);
-                    // metadata.GetTypeDefinition(attrType.ResolutionScope)
-
-                    if (metadata.GetString(attrType.Name) is not "DebuggableAttribute")
-                    {
-                        continue;
-                    }
-                    var sig = metadata.GetBlobBytes(customAttribute.Value);
-                    Console.WriteLine("BitConverter: "+ BitConverter.ToInt32(sig, 0));
-                    if (sig.Length != 8)
-                    {
-                        Console.WriteLine("Expected 8 bytes");
-                    }
-                    else
-                    {
-                        if (sig[2] == 7 && sig[3] == 1)
-                        {
-                            Log.LogMessage(MessageImportance.High, "Unoptimized assembly detected: " + assemblyPath);
-                        }
-                        else
-                        {
-                            Debug.Assert(sig[2] == 2 && sig[3] == 0);
-                            Log.LogMessage(MessageImportance.Low, "Optimized assembly detected: " + assemblyPath);
-                        }
-                    }
-                    var provider = new CustomAttributeTypeProvider();
-
-                    CustomAttributeValue<string> value = customAttribute.DecodeValue(provider);
-                    foreach (var customAttributeTypedArgument in value.FixedArguments)
-                    {
-                        var bitmask = int.Parse(customAttributeTypedArgument.Value!.ToString());
-                        var isJitOptimizerDisabled = (bitmask & (int) DebuggableAttribute.DebuggingModes.DisableOptimizations) != 0;
-                        // https://github.com/dotnet/runtime/blob/478571ca82dedc4f07f6a176709224adf3ee367a/src/libraries/System.Private.CoreLib/src/System/Diagnostics/DebuggableAttribute.cs#L49
-                        Console.WriteLine("IsJITOptimizerDisabled " + isJitOptimizerDisabled);
-                    }
+                    // https://github.com/dotnet/runtime/blob/478571ca82dedc4f07f6a176709224adf3ee367a/src/libraries/System.Private.CoreLib/src/System/Diagnostics/DebuggableAttribute.cs#L49
+                    var isJitOptimizerDisabled = (bitmask & (int) DebuggableAttribute.DebuggingModes.DisableOptimizations) != 0;
+                    Log.LogMessage(MessageImportance.Low, "DebuggableAttribute flags for: {0} is {1}. IsJitOptimizerDisabled: {2}.", file, bitmask, isJitOptimizerDisabled);
+                    isOptimized = !isJitOptimizerDisabled;
                 }
 
-                // var asm = Assembly.ReflectionOnlyLoadFrom(assemblyPath);
-                // if (!asm.IsOptimized())
-                // {
-                //     Log.LogMessage(MessageImportance.High, "Unoptimized assembly detected: " + assemblyPath);
-                // }
-                // else
-                // {
-                //     Log.LogMessage(MessageImportance.High, "Optimized assembly detected: " + assemblyPath);
-                // }
+                if (isOptimized)
+                {
+                    Log.LogMessage(MessageImportance.Low, "Optimized assembly detected: " + assemblyPath);
+                }
+                else
+                {
+                    Log.LogWarning("Unoptimized assembly detected: {0}{1}{2}", file, Environment.NewLine, assemblyPath);
+                }
             }
             return true;
         }
     }
-    
+
     internal class CustomAttributeTypeProvider : DisassemblingTypeProvider, ICustomAttributeTypeProvider<string>
     {
-        public string GetSystemType()
-        {
-            return "[System.Runtime]System.Type";
-        }
+        public string GetSystemType() => "[System.Runtime]System.Type";
 
-        public bool IsSystemType(string type)
-        {
-            return type == "[System.Runtime]System.Type"  // encountered as typeref
-                   || Type.GetType(type) == typeof(Type);    // encountered as serialized to reflection notation
-        }
+        public bool IsSystemType(string type) => type == "[System.Runtime]System.Type" || Type.GetType(type) == typeof(Type);
 
-        public string GetTypeFromSerializedName(string name)
-        {
-            return name;
-        }
+        public string GetTypeFromSerializedName(string name) => name;
 
-        public PrimitiveTypeCode GetUnderlyingEnumType(string type)
-        {
-            return PrimitiveTypeCode.Int32;
-        }
+        public PrimitiveTypeCode GetUnderlyingEnumType(string type) => PrimitiveTypeCode.Int32;
     }
 
     internal class DisassemblingGenericContext
@@ -152,7 +114,6 @@ namespace UnoptimizedAssemblyDetector
     {
         public virtual string GetPrimitiveType(PrimitiveTypeCode typeCode)
         {
-            Console.WriteLine("GetPrimitiveType: " + typeCode);
             switch (typeCode)
             {
                 case PrimitiveTypeCode.Boolean:
@@ -210,32 +171,25 @@ namespace UnoptimizedAssemblyDetector
                     return "void";
 
                 default:
-                    Debug.Assert(false);
+                    Debug.Fail("typeCode unknown");
                     throw new ArgumentOutOfRangeException(nameof(typeCode));
             }
         }
 
         public virtual string GetTypeFromDefinition(MetadataReader reader, TypeDefinitionHandle handle, byte rawTypeKind = 0)
         {
-            TypeDefinition definition = reader.GetTypeDefinition(handle);
+            var definition = reader.GetTypeDefinition(handle);
 
             string name = definition.Namespace.IsNil
                 ? reader.GetString(definition.Name)
                 : reader.GetString(definition.Namespace) + "." + reader.GetString(definition.Name);
-            Console.WriteLine("GetTypeFromDefinition: " + name);
-
-            // if (definition.Attributes.IsNested())
-            // {
-            //     TypeDefinitionHandle declaringTypeHandle = definition.GetDeclaringType();
-            //     return GetTypeFromDefinition(reader, declaringTypeHandle, 0) + "/" + name;
-            // }
 
             return name;
         }
 
         public virtual string GetTypeFromReference(MetadataReader reader, TypeReferenceHandle handle, byte rawTypeKind = 0)
         {
-            TypeReference reference = reader.GetTypeReference(handle);
+            var reference = reader.GetTypeReference(handle);
             Handle scope = reference.ResolutionScope;
 
             string name = reference.Namespace.IsNil
@@ -265,44 +219,24 @@ namespace UnoptimizedAssemblyDetector
         }
 
         public virtual string GetTypeFromSpecification(MetadataReader reader, DisassemblingGenericContext genericContext, TypeSpecificationHandle handle, byte rawTypeKind = 0)
-        {
-            return reader.GetTypeSpecification(handle).DecodeSignature(this, genericContext);
-        }
+            => reader.GetTypeSpecification(handle).DecodeSignature(this, genericContext);
 
-        public virtual string GetSZArrayType(string elementType)
-        {
-            return elementType + "[]";
-        }
+        public virtual string GetSZArrayType(string elementType) => elementType + "[]";
 
-        public virtual string GetPointerType(string elementType)
-        {
-            return elementType + "*";
-        }
+        public virtual string GetPointerType(string elementType) => elementType + "*";
 
-        public virtual string GetByReferenceType(string elementType)
-        {
-            return elementType + "&";
-        }
+        public virtual string GetByReferenceType(string elementType) => elementType + "&";
 
         public virtual string GetGenericMethodParameter(DisassemblingGenericContext genericContext, int index)
-        {
-            return "!!" + genericContext.MethodParameters[index];
-        }
+            => "!!" + genericContext.MethodParameters[index];
 
         public virtual string GetGenericTypeParameter(DisassemblingGenericContext genericContext, int index)
-        {
-            return "!" + genericContext.TypeParameters[index];
-        }
+            => "!" + genericContext.TypeParameters[index];
 
-        public virtual string GetPinnedType(string elementType)
-        {
-            return elementType + " pinned";
-        }
+        public virtual string GetPinnedType(string elementType) => elementType + " pinned";
 
         public virtual string GetGenericInstantiation(string genericType, ImmutableArray<string> typeArguments)
-        {
-            return genericType + "<" + string.Join(",", typeArguments) + ">";
-        }
+            => genericType + "<" + string.Join(",", typeArguments) + ">";
 
         public virtual string GetArrayType(string elementType, ArrayShape shape)
         {
@@ -311,9 +245,9 @@ namespace UnoptimizedAssemblyDetector
             builder.Append(elementType);
             builder.Append('[');
 
-            for (int i = 0; i < shape.Rank; i++)
+            for (var i = 0; i < shape.Rank; i++)
             {
-                int lowerBound = 0;
+                var lowerBound = 0;
 
                 if (i < shape.LowerBounds.Length)
                 {
@@ -338,34 +272,14 @@ namespace UnoptimizedAssemblyDetector
             return builder.ToString();
         }
 
-        public virtual string GetTypeFromHandle(MetadataReader reader, DisassemblingGenericContext genericContext, EntityHandle handle)
-        {
-            switch (handle.Kind)
-            {
-                case HandleKind.TypeDefinition:
-                    return GetTypeFromDefinition(reader, (TypeDefinitionHandle)handle);
-
-                case HandleKind.TypeReference:
-                    return GetTypeFromReference(reader, (TypeReferenceHandle)handle);
-
-                case HandleKind.TypeSpecification:
-                    return GetTypeFromSpecification(reader, genericContext, (TypeSpecificationHandle)handle);
-
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(handle));
-            }
-        }
-
         public virtual string GetModifiedType(string modifierType, string unmodifiedType, bool isRequired)
-        {
-            return unmodifiedType + (isRequired ? " modreq(" : " modopt(") + modifierType + ")";
-        }
+            => unmodifiedType + (isRequired ? " modreq(" : " modopt(") + modifierType + ")";
 
         public virtual string GetFunctionPointerType(MethodSignature<string> signature)
         {
-            ImmutableArray<string> parameterTypes = signature.ParameterTypes;
+            var parameterTypes = signature.ParameterTypes;
 
-            int requiredParameterCount = signature.RequiredParameterCount;
+            var requiredParameterCount = signature.RequiredParameterCount;
 
             var builder = new StringBuilder();
             builder.Append("method ");
@@ -397,51 +311,6 @@ namespace UnoptimizedAssemblyDetector
 
             builder.Append(')');
             return builder.ToString();
-        }
-    }
-    
-    internal sealed class StringParameterValueTypeProvider : ISignatureTypeProvider<string, object?>
-    {
-        private readonly BlobReader valueReader;
-
-        public StringParameterValueTypeProvider(MetadataReader reader, BlobHandle value)
-        {
-            Reader = reader;
-            valueReader = reader.GetBlobReader(value);
-
-            var prolog = valueReader.ReadUInt16();
-            if (prolog != 1) throw new BadImageFormatException("Invalid custom attribute prolog.");
-        }
-
-        public MetadataReader Reader { get; }
-
-        public string GetArrayType(string elementType, ArrayShape shape) => "";
-        public string GetByReferenceType(string elementType) => "";
-        public string GetFunctionPointerType(MethodSignature<string> signature) => "";
-        public string GetGenericInstance(string genericType, ImmutableArray<string> typestrings) => "";
-        public string GetGenericInstantiation(string genericType, ImmutableArray<string> typeArguments) { throw new NotImplementedException(); }
-        public string GetGenericMethodParameter(int index) => "";
-        public string GetGenericMethodParameter(object? genericContext, int index) { throw new NotImplementedException(); }
-        public string GetGenericTypeParameter(int index) => "";
-        public string GetGenericTypeParameter(object? genericContext, int index) { throw new NotImplementedException(); }
-        public string GetModifiedType(string modifier, string unmodifiedType, bool isRequired) => "";
-        public string GetPinnedType(string elementType) => "";
-        public string GetPointerType(string elementType) => "";
-        public string GetPrimitiveType(PrimitiveTypeCode typeCode)
-        {
-            if (typeCode == PrimitiveTypeCode.String) return valueReader.ReadSerializedString() ?? "";
-            return "";
-        }
-        public string GetSZArrayType(string elementType) => "";
-        public string GetTypeFromDefinition(MetadataReader reader, TypeDefinitionHandle handle, byte rawTypeKind) => "";
-        public string GetTypeFromReference(MetadataReader reader, TypeReferenceHandle handle, byte rawTypeKind) => "";
-        public string GetTypeFromSpecification(MetadataReader reader, object? genericContext, TypeSpecificationHandle handle, byte rawTypeKind) => "";
-    }
-    internal static class MetadataHelper
-    {
-        public static string? ToString(this StringHandle handle, MetadataReader reader)
-        {
-            return handle.IsNil ? null : reader.GetString(handle);
         }
     }
 }
