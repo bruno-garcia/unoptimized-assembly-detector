4fea82941e7a1553fb5c27eb651135e2edb9de65
Bruno Garcia <bruno@brunogarcia.com>
2021-05-20 17:50:11 -0400

    test on *nix

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@ rm -rf ~/.nuget/packages/unoptimizedassemblydetector/
 rm -rf ~/nuget-local-feed/packages/unoptimizedassemblydetector/
 rm -f ./nupkg/*.nupkg
 
-dotnet clean -nologo
-
 dotnet pack -c release -nologo src/UnoptimizedAssemblyDetector/UnoptimizedAssemblyDetector.csproj
 
 dotnet build -c release -f net461 sample/Sample/Sample.csproj
---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eu
+
+# ./build.sh
+
+test() {
+  if ! echo $1 | grep "warning : Unoptimized assembly detected: 'EmptyLibDebug.dll'"
+  then
+    echo "Didn't detect EmptyLibDebug as unoptimized"
+    exit -1
+  fi
+  if ! echo $1 | grep "warning : Unoptimized assembly detected: 'StructureMap.dll'"
+  then
+    echo "Didn't detect StructureMap as unoptimized"
+    exit -1
+  fi
+}
+
+test "$(dotnet build -c release -f net461 sample/Sample/Sample.csproj)"
+test "$(dotnet build -c release -f netcoreapp2.1 sample/Sample/Sample.csproj)"
